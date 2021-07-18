abb2bb1600b8946afc7a97a92234f8dbf6e2e276
Wencke Liermann <wencke.liermann@uni-potsdam.de>
2021-07-18 17:35:47 +0200

    Remove comment about Bearer prefix, add default port to call example

--- minimal/README.md
+++ minimal/README.md
@@ -5,7 +5,7 @@ The bot is composed of a single event handler but others can be added in a simil
 
 To run the bot, you can run a command in a similar fashion as:
 ```bash
-docker run -e SLURK_TOKEN=d8fb2d9a-8afe-43c3-87b5-2c934596907f -e SLURK_USER=2 --net="host" slurk/minimal-bot
+docker run -e SLURK_TOKEN=d8fb2d9a-8afe-43c3-87b5-2c934596907f -e SLURK_USER=2 -e SLURK_PORT=5000 --net="host" slurk/minimal-bot
 ```
 
 The token has to be linked to a permissions entry that gives the bot at least the following rights: `api`, `send_message`
--- minimal/minimal.py
+++ minimal/minimal.py
@@ -37,18 +37,17 @@ class MinimalBot:
         self.user = user
         self.uri = host
         if port is not None:
-            self.uri += f':{port}'
-        self.uri += '/slurk/api'
+            self.uri += f":{port}"
+        self.uri += "/slurk/api"
         # register all event handlers
         self.register_callbacks()
 
     def run(self):
         # establish a connection to the server
-        # important to specify the token as it is i.e. no Bearer prefix
         self.sio.connect(
             self.uri,
-            headers={'Authorization': self.token, 'user': self.user},
-            namespaces='/'
+            headers={"Authorization": self.token, "user": self.user},
+            namespaces="/",
         )
         # wait until the connection with the server ends
         self.sio.wait()
@@ -57,76 +56,74 @@ class MinimalBot:
         @self.sio.event
         def joined_room(data):
             # get the id of the room that was entered
-            room_id = data['room']
+            room_id = data["room"]
             # get the id of the user that just joined a room
-            user_id = data['user']
-            LOG.debug(f'User {user_id} entered the room {room_id}')
+            user_id = data["user"]
+            LOG.debug(f"User {user_id} entered the room {room_id}")
 
             # based on the id retrieve additional information
             # with a running slurk server all such requests possible can
             # be looked at under http://localhost/rapidoc
             response = requests.get(
-                f'{self.uri}/users/{user_id}',
-                headers={'Authorization': f'Bearer {self.token}'}
+                f"{self.uri}/users/{user_id}",
+                headers={"Authorization": f"Bearer {self.token}"},
             )
 
             # verify that the request was successful
             if not response.ok:
-                LOG.error(f'Could not get user details due to status {response.status_code}.')
+                LOG.error(
+                    f"Could not get user details due to status {response.status_code}."
+                )
             else:
                 # read out information delivered with the response
                 user = response.json()
                 self.sio.emit(
-                    'text',
-                    {'msg': f'Hi^o^ I am a {user["name"]}!', 'room': room_id}
+                    "text", {"msg": f'Hi^o^ I am a {user["name"]}!', "room": room_id}
                 )
 
             # retrieve all log entries for this room and user
             response = requests.get(
-                f'{self.uri}/rooms/{room_id}/users/{user_id}/logs',
-                headers={'Authorization': f"Bearer {self.token}"}
+                f"{self.uri}/rooms/{room_id}/users/{user_id}/logs",
+                headers={"Authorization": f"Bearer {self.token}"},
             )
             if not response.ok:
-                LOG.error(f'Could not get logs due to status {response.status_code}.')
+                LOG.error(f"Could not get logs due to status {response.status_code}.")
             else:
                 logs = response.json()
                 for log_entry in logs:
-                    logging.info(f'- status: {log_entry["event"]}, data: {log_entry["data"]}')
+                    logging.info(
+                        f'- status: {log_entry["event"]}, data: {log_entry["data"]}'
+                    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # set up logging configuration
-    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
+    logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(message)s")
 
     # create commandline parser
-    parser = argparse.ArgumentParser(description='Run Minimal Bot.')
+    parser = argparse.ArgumentParser(description="Run Minimal Bot.")
 
     # collect environment variables as defaults
-    if 'SLURK_TOKEN' in os.environ:
-        token = {'default': os.environ['SLURK_TOKEN']}
+    if "SLURK_TOKEN" in os.environ:
+        token = {"default": os.environ["SLURK_TOKEN"]}
     else:
-        token = {'required': True}
-    if 'SLURK_USER' in os.environ:
-        user = {'default': os.environ['SLURK_USER']}
+        token = {"required": True}
+    if "SLURK_USER" in os.environ:
+        user = {"default": os.environ["SLURK_USER"]}
     else:
-        user = {'required': True}
-    host = {'default': os.environ.get('SLURK_HOST', 'http://localhost')}
-    port = {'default': os.environ.get('SLURK_PORT')}
+        user = {"required": True}
+    host = {"default": os.environ.get("SLURK_HOST", "http://localhost")}
+    port = {"default": os.environ.get("SLURK_PORT")}
 
     # register commandline arguments
-    parser.add_argument('-t', '--token',
-                        help='token for logging in as bot (see SERVURL/token)',
-                        **token)
-    parser.add_argument('-u', '--user',
-                        help='user id for the bot',
-                        **user)
-    parser.add_argument('-c', '--host',
-                        help='full URL (protocol, hostname) of chat server',
-                        **host)
-    parser.add_argument('-p', '--port',
-                        type=int,
-                        help='port of chat server',
-                        **port)
+    parser.add_argument(
+        "-t", "--token", help="token for logging in as bot (see SERVURL/token)", **token
+    )
+    parser.add_argument("-u", "--user", help="user id for the bot", **user)
+    parser.add_argument(
+        "-c", "--host", help="full URL (protocol, hostname) of chat server", **host
+    )
+    parser.add_argument("-p", "--port", type=int, help="port of chat server", **port)
     args = parser.parse_args()
 
     # create bot instance
