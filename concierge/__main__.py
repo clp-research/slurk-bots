50ec6b876c6cd695c520c95d47b5998e4d3de826
Wencke Liermann <wencke.liermann@uni-potsdam.de>
2021-12-21 15:49:28 +0100

    Let echo and concierge bot inherit from parent

--- concierge/__main__.py
+++ concierge/__main__.py
@@ -1,89 +1,31 @@
-import argparse
 import logging
-import os
 
 import requests
-import socketio
+
+from templates import Bot
 
 
-LOG = logging.getLogger(__name__)
-
-
-class ConciergeBot:
-    sio = socketio.Client(logger=True)
-    tasks = dict()
-
+class ConciergeBot(Bot):
     def __init__(self, token, user, host, port):
         """This bot lists users joining a designated
         waiting room and sends a group of users to a task room
         as soon as the minimal number of users needed for the
         task is reached.
-
-        :param token: A uuid; a string following the same pattern
-            as `0c45b30f-d049-43d1-b80d-e3c3a3ca22a0`
-        :type token: str
-        :param user: ID of a `User` object that was created with
-        the token.
-        :type user: int
-        :param host: Full URL including protocol and hostname.
-        :type host: str
-        :param port: Port used by the slurk chat server.
-        :type port: int
         """
-        self.token = token
-        self.user = user
-        self.uri = host
-        if port is not None:
-            self.uri += f":{port}"
-        self.uri += "/slurk/api"
-
-        LOG.info(f"Running concierge bot on {self.uri} with token {self.token}")
-        # register all event handlers
-        self.register_callbacks()
-
-    def run(self):
-        # establish a connection to the server
-        self.sio.connect(
-            self.uri,
-            headers={"Authorization": f"Bearer {self.token}", "user": self.user},
-            namespaces="/",
-        )
-        # wait until the connection with the server ends
-        self.sio.wait()
+        super().__init__(token, user, host, port)
+        self.tasks = dict()
 
     def register_callbacks(self):
         @self.sio.event
         def status(data):
+            user = data["user"]
+            task = self.get_user_task(user)
             if data["type"] == "join":
-                user = data["user"]
-                task = self.get_user_task(user)
                 if task:
                     self.user_task_join(user, task, data["room"])
             elif data["type"] == "leave":
-                user = data["user"]
-                task = self.get_user_task(user)
                 if task:
                     self.user_task_leave(user, task)
-
-    @staticmethod
-    def message_callback(success, error_msg=None):
-        """Is passed as an optional argument to a server emit.
-
-        Will be invoked after the server has processed the event,
-        any values returned by the event handler will be passed
-        as arguments.
-
-        :param success: `True` if the message was successfully sent,
-            else `False`.
-        :type success: bool
-        :param error_msg: Reason for an insuccessful message
-            transmission. Defaults to None.
-        :type status: str, optional
-        """
-        if not success:
-            LOG.error(f"Could not send message: {error_msg}")
-            exit(1)
-        LOG.debug("Sent message successfully.")
 
     def get_user_task(self, user):
         """Retrieve task assigned to user.
@@ -95,10 +37,7 @@ class ConciergeBot:
             f'{self.uri}/users/{user["id"]}/task',
             headers={"Authorization": f"Bearer {self.token}"}
         )
-        if not task.ok:
-            LOG.error(f"Could not get task: {task.status_code}")
-            exit(2)
-        LOG.debug("Got user task successfully.")
+        self.request_feedback(task, "ConciergeBot requests user task")
         return task.json()
 
     def create_room(self, layout_id):
@@ -112,10 +51,7 @@ class ConciergeBot:
             headers={"Authorization": f"Bearer {self.token}"},
             json={"layout_id": layout_id}
         )
-        if not room.ok:
-            LOG.error(f"Could not create task room: {room.status_code}")
-            exit(3)
-        LOG.debug("Created room successfully.")
+        self.request_feedback(room, "ConciergeBot creates task room")
         return room.json()
 
     def join_room(self, user_id, room_id):
@@ -130,10 +66,7 @@ class ConciergeBot:
             f"{self.uri}/users/{user_id}/rooms/{room_id}",
             headers={"Authorization": f"Bearer {self.token}"}
         )
-        if not response.ok:
-            LOG.error(f"Could not let user join room: {response.status_code}")
-            exit(4)
-        LOG.debug("Sending user to new room was successful.")
+        self.request_feedback(response, "ConciergeBot sends user to task room")
         return response.headers["ETag"]
 
     def delete_room(self, user_id, room_id, etag):
@@ -151,12 +84,9 @@ class ConciergeBot:
             headers={"Authorization": f"Bearer {self.token}",
                      "If-Match": etag}
         )
-        if not response.ok:
-            LOG.error(f"Could not remove user from room: {response.status_code}")
-            exit(5)
-        LOG.debug("Removing user from room was successful.")
+        self.request_feedback(response, "ConciergeBot removes user from waiting room")
 
-    def user_task_join(self, user, task, room):
+    def user_task_join(self, user, task, room_id):
         """A connected user and their task are registered.
 
         Once the final user necessary to start a task
@@ -168,19 +98,19 @@ class ConciergeBot:
         :param task: Holds keys `date_created`, `date_modified`, `id`,
             `layout_id`, `name` and `num_users`.
         :type task: dict
-        :param room: Identifier of a room that the user joined.
-        :type room: str
+        :param room_id: Identifier of a room that the user joined.
+        :type room_id: str
         """
         task_id = task["id"]
         user_id = user["id"]
         user_name = user["name"]
         # register task together with the user_id
-        self.tasks.setdefault(task_id, {})[user_id] = room
+        self.tasks.setdefault(task_id, {})[user_id] = room_id
 
         if len(self.tasks[task_id]) == task["num_users"]:
             new_room = self.create_room(task["layout_id"])
             # list cast necessary because the dictionary is actively altered
-            # due to parallely received "leave" events
+            # due to parallel received "leave" events
             for user_id, old_room_id in list(self.tasks[task_id].items()):
                 etag = self.join_room(user_id, new_room["id"])
                 self.delete_room(user_id, old_room_id, etag)
@@ -195,7 +125,7 @@ class ConciergeBot:
                         "I am looking for a partner for you, it might take "
                         "some time, so be patient, please...",
                     "receiver_id": user_id,
-                    "room": room,
+                    "room": room_id,
                     "html": True
                 },
                 callback=self.message_callback
@@ -221,29 +151,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(message)s")
 
     # create commandline parser
-    parser = argparse.ArgumentParser(description="Run Concierge Bot.")
-
-    # collect environment variables as defaults
-    if "SLURK_TOKEN" in os.environ:
-        token = {"default": os.environ["SLURK_TOKEN"]}
-    else:
-        token = {"required": True}
-    if "SLURK_USER" in os.environ:
-        user = {"default": os.environ["SLURK_USER"]}
-    else:
-        user = {"required": True}
-    host = {"default": os.environ.get("SLURK_HOST", "http://localhost")}
-    port = {"default": os.environ.get("SLURK_PORT")}
-
-    # register commandline arguments
-    parser.add_argument(
-        "-t", "--token", help="token for logging in as bot (see SERVURL/token)", **token
-    )
-    parser.add_argument("-u", "--user", help="user id for the bot", **user)
-    parser.add_argument(
-        "-c", "--host", help="full URL (protocol, hostname) of chat server", **host
-    )
-    parser.add_argument("-p", "--port", type=int, help="port of chat server", **port)
+    parser = ConciergeBot.create_argparser()
     args = parser.parse_args()
 
     # create bot instance
--- echo/README.md
+++ echo/README.md
@@ -1,13 +1,19 @@
 ## Echo Bot
+This bot parrots user messages. If a private message is sent to the bot by a user, the bot will send it back to this individual user only. Other messages are sent back to the room from where they were received.
 
-This is a bot that repeats what a user typed by sending it back to the individual user (for private messages) or their room. The bot is composed of three methods:
-* `on_new_task_room`: triggered once a new task room is created. The bot will join this room if the task id matches the one it is called with.
-* `on_text_message`: triggered once a message is sent that the bot can see. If the text message is private, broadcast it back only to the user. If it is public, broadcast it back to the room.
-* `on_image_message`: triggered once an image is sent that the bot can see. If the image message is private, broadcast it back only to the user. If it is public, broadcast it back to the room.
+* `on_text_message`: triggered for messages containing text
+* `on_image_message`: triggered for messages containing images
 
+**Run with docker (recommended)**
 The bot can be run with a command that follows the pattern:
 ```bash
-docker run -e SLURK_TOKEN=c8469175-8e34-4c69-a2c7-472edce424b3 -e SLURK_USER=1 -e SLURK_PORT=5000 -e ECHO_TASK_ID=1 --net="host" slurk/echo-bot
+docker run -e SLURK_TOKEN=24e43285-c8ba-44e3-8608-3d91227c9da7 -e SLURK_USER=1 -e SLURK_PORT=5000 -e ECHO_TASK_ID=1 --net="host" slurk/echo-bot
+```
+
+**Run without docker**
+Without docker, bots need to be started from the *slurk-bots* repository as follows:
+```bash
+python -m echo --token 24e43285-c8ba-44e3-8608-3d91227c9da7 --user 1 --port 5000 --task 1
 ```
 
 The token needs to have at least the permissions `api`, `send_privately`, `send_message` and `send_image`. Please refer to <https://clp-research.github.io/slurk/slurk_multibots.html> for more detailed information.
