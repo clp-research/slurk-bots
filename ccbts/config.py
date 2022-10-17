a60420a09af6d85928d1cb5309b4a1d782e030ab
seba <seba.g90@gmail.com>
2022-10-17 13:19:34 +0200

    new structure with data folder, images are from cocobot

--- ccbts/config.py
+++ ccbts/config.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """File contains global variables meant to be used read-only."""
 
-import os
 from pathlib import Path
 
 ROOT = Path().resolve() #os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-TASK_GREETING = Path(f"{ROOT}/ccbts/task_greeting.txt").read_text().strip().split("\n\n")
+TASK_GREETING = Path(f"{ROOT}/ccbts/data/task_greeting.txt").read_text().strip().split("\n\n")
 
 INSTRUCTIONS = dict(
-    player=Path(f"{ROOT}/ccbts/instruction_player.txt").read_text().strip(),
-    wizard=Path(f"{ROOT}/ccbts/instruction_wizard.txt").read_text().strip()
+    player=Path(f"{ROOT}/ccbts/data/instruction_player.txt").read_text().strip(),
+    wizard=Path(f"{ROOT}/ccbts/data/instruction_wizard.txt").read_text().strip()
 )
 
-IMGS = Path(f"{ROOT}/ccbts/task_greeting.txt").read_text().strip().split("\n")
+IMGS = [image for image in Path(f"{ROOT}/ccbts/data/images").iterdir()]
