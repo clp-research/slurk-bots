5c86b23039ac0171fa69563cbdf1c306c7208217
seba <seba.g90@gmail.com>
2022-11-16 15:49:43 +0100

    added instruction and legend at the beginning

--- golmieval/config.py
+++ golmieval/config.py
@@ -7,12 +7,16 @@ ROOT = Path().resolve() #os.path.dirname(os.path.dirname(os.path.abspath(__file_
 
 
 TASK_GREETING = Path(f"{ROOT}/golmieval/data/task_greeting.txt").read_text().strip().split("\n\n")
+TASK_INSTR = Path(f"{ROOT}/golmieval/data/instruction.txt").read_text().strip()
 
-INSTRUCTIONS = dict(
-    player=Path(f"{ROOT}/golmieval/data/instruction_player.txt").read_text().strip(),
-    wizard=Path(f"{ROOT}/golmieval/data/instruction_wizard.txt").read_text().strip()
-)
 
 BOARDS = Path(f"{ROOT}/golmieval/data/boards.jsonl")
-
 BOARDS_PER_ROOM = 9
+
+
+COLOR_MESSAGE = '<a style="color:{color};">{message}</a>'
+STANDARD_COLOR = "Purple"
+WARNING_COLOR = "FireBrick"
+
+
+TYPES = "https://raw.githubusercontent.com/clp-research/golmi/exp-descrimage/app/descrimage/static/types.png"
