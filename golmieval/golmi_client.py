545244cbaf7381e7170b58cb46a915c7664d167d
seba <seba.g90@gmail.com>
2022-11-27 18:06:01 +0100

    add possibility to click on the piece to know if that's the target

--- golmieval/golmi_client.py
+++ golmieval/golmi_client.py
@@ -18,49 +18,13 @@ class MyCustomNamespace(socketio.AsyncNamespace):
 
 
 class GolmiClient:
-    def __init__(self, address, slurk_socket, room_id):
+    def __init__(self, slurk_socket):
         self.socket = socketio.Client()
-        self.address = address
-        self.room_id = room_id
         self.slurk_socket = slurk_socket
 
-    # def call_backs(self):
-    #     @self.socket.on("joined_room")
-    #     def receive(data):
-    #         self.slurk_socket.emit(
-    #             "text", {"message": "joined", "room": self.room_id, "html": True}
-    #         )
-
-    #     @self.socket.on("update_config")
-    #     def update_config(data):
-    #         self.slurk_socket.emit(
-    #             "text", {"message": "update config", "room": self.room_id, "html": True}
-    #         )
-
-    #     @self.socket.on("update_state")
-    #     def update_state(data):
-    #         self.state = data
-    #         self.slurk_socket.emit(
-    #             "text", {"message": "update_state", "room": self.room_id, "html": True}
-    #         )
-
-    #     @self.socket.on("update_objs")
-    #     def update_objs(data):
-    #         self.state["objs"] = data
-
-    #     @self.socket.on("update_grippers")
-    #     def update_grippers(data):
-    #         self.state["grippers"] = data
-
-    #         for gripper in data.values():
-    #             if gripper["gripped"] != None:
-    #                 for idn, obj in gripper["gripped"].items():
-    #                     self.state["objs"][idn] = obj
-
-    def run(self, auth):
-        # self.call_backs()
-        self.socket.connect(self.address, auth={"password": auth})
-        self.socket.call("join", {"room_id": self.room_id})
+    def run(self, address, room_id, auth):
+        self.socket.connect(address, auth={"password": auth})
+        self.socket.call("join", {"room_id": room_id})
 
     def random_init(self, random_config):
         self.socket.emit("random_init", random_config)
