f27e623cc35cbb9f600e42d473ca9c380de63462
seba <seba.g90@gmail.com>
2022-12-01 13:37:09 +0100

    add dataloader from golmievalbot

---
+++ golmi/dataloader.py
@@ -0,0 +1,48 @@
+from itertools import cycle
+import json
+import random
+
+
+class Dataloader(dict):
+    def __init__(self, path, n):
+        self._path = path
+        self._n = n
+        self._images = None
+
+    def _sample_boards(self):
+        baords = self._read_board_file()
+        images_per_level = self._n // 3
+        sample = list()
+        for level in cycle(["easy", "medium", "hard"]):
+            for board in random.sample(baords[level], images_per_level):
+                sample.append(board)
+
+                if len(sample) == self._n:
+                    return sample            
+
+    def _read_board_file(self):
+        """read boards and divide by level"""
+        boards = dict(
+            easy=list(),
+            medium=list(),
+            hard=list()
+        )
+        with self._path.open('r', encoding="utf-8") as infile:
+            for line in infile:
+                board = json.loads(line)
+                level = board["board_info"]["difficoulty"]
+
+                # select target
+                state = board["state"]
+                target_id = str(board["target"])
+                target_obj = state["objs"][target_id]
+                state["targets"][target_id] = target_obj
+
+                boards[level].append(board)
+        return boards
+
+    def get_boards(self, room_id):
+        """sample random boards for a room"""
+        room_boards = self._sample_boards()
+        random.shuffle(room_boards)
+        self[room_id] = room_boards
