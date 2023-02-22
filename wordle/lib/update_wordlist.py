f7017323bdbede70061e116f74497d6301aaef10
Tamara Atanasoska <atanasoska.tamara@gmail.com>
2023-02-22 11:56:58 +0100

    Adding a script to update the wordlist

---
+++ wordle/lib/update_wordlist.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def main():
+    # import the pairs file
+    df1 = pd.read_csv("../data/image_data.tsv", sep="\t", header=None)
+    df1.columns = ["term", "image_path"]
+
+    # import the existing wordslist
+    df2 = pd.read_csv("../data/wordlist.txt", sep="\n", header=None)
+    df2.columns = ["term"]
+
+    # update the existing wordslist
+    df3 = pd.concat([df1["term"], df2["term"]]).drop_duplicates().reset_index(drop=True)
+
+    # sort it alphabetically and replace it
+    df3 = df3.sort_values()
+    df3.to_csv("../data/wordlist.txt", sep="\n", index=False, header=False)
+
+    print("Wordlist updated!")
+    print(
+        "The wordlist has gained "
+        + str(len(df3.index) - len(df2.index))
+        + " new entries."
+    )
+
+
+if __name__ == "__main__":
+    main()
