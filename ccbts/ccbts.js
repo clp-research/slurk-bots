5a7961e32ce03cc94914cbe9ab206c134b6cf42f
seba <seba.g90@gmail.com>
2023-03-17 15:20:40 +0100

    update js

--- ccbts/ccbts.js
+++ ccbts/ccbts.js
@@ -27,17 +27,23 @@ function start_golmi(url, password, role, room_id) {
     );
 
     golmi_socket_working.connect()
-    golmi_socket_working.emit("join", { "room_id": `${room_id}_s` });
+    golmi_socket_working.emit("join", { "room_id": `${room_id}_w` });
 
 
     if (role === "wizard"){
         // if the gripper is used there is no need to track clicks
         grLayer.onclick = (event) => {
-            console.log(event)
             socket.emit("mouse", {
                 type: "click",
-                coordinates: {"x": event.offsetX, "y": event.offsetY, "block_size": sourcelayerView.blockSize},
-                element_id: "gripper",
+                coordinates: {
+                    event: "click",
+                    x: event.offsetX,
+                    y: event.offsetY,
+                    block_size: workinglayerView.blockSize,
+                    color: get_propery("color"),
+                    shape: get_propery("shape"),
+                    action: get_propery("action"),
+                },
                 room: self_room
             });
         }
@@ -105,6 +111,31 @@ function reset_role(description) {
 }
 
 
+function on_checkbox_change(this_element){
+    clear_others(this_element);
+    parsed = this_element.split("_")
+    value = parsed[1]
+
+    if ($(`#action_place`).is(':checked') === true){
+        return
+    }
+
+    // we allow to change the color
+    if (["red", "blue", "yellow", "green"].includes(value)){
+        socket.emit("message_command",
+            {
+                command: {
+                    event: "update_object",
+                    shape: get_propery("shape"),
+                    color: get_propery("color"),
+                },
+                room: self_room
+            }
+        )
+    }
+}
+
+
 function clear_others(this_element){
     options = {
         color: new Set(["red", "blue", "yellow", "green"]),
@@ -125,6 +156,20 @@ function clear_others(this_element){
     })
 }
 
+
+function get_propery(this_element){
+    options = {
+        color: ["red", "blue", "yellow", "green"],
+        shape: ["screw", "bridge", "nut", "washer"],
+        action: ["select", "place"]
+    }
+
+    for (let item of options[this_element]){
+        if ($(`#${this_element}_${item}`).is(':checked')){
+            return item
+        }
+    }
+}
 
 $(document).ready(() => {
     socket.on("command", (data) => {
@@ -167,6 +212,15 @@ $(document).ready(() => {
                     $("#history").scrollTop($("#history")[0].scrollHeight);
                     $("#input").val(to_run.join("\n"))
                     break;
+
+                case "update_selectors":
+                    shape = data.command.shape
+                    color = data.command.color
+
+                    $(`#shape_${shape}`).prop('checked', true);
+                    $(`#color_${color}`).prop('checked', true);
+                    clear_others(`shape_${shape}`)
+                    clear_others(`color_${color}`)
             }
         }
     });
