5b77c8ef3a781614de7a04f57b5df76b7ed1ad04
seba <seba.g90@gmail.com>
2023-05-03 21:27:23 +0200

    remove unused variables

--- coco/config.py
+++ coco/config.py
@@ -18,7 +18,6 @@ INSTRUCTIONS = dict(
 )
 
 
-
 COLOR_MESSAGE = '<a style="color:{color};">{message}</a>'
 STANDARD_COLOR = "Purple"
 WARNING_COLOR = "FireBrick"
@@ -28,53 +27,6 @@ TIME_LEFT = 5  # how many minutes a user can stay in a room before closing it
 
 
 CONFIG = {"width": 7.0, "height": 7.0, "move_step": 1, "prevent_overlap": False}
-OBJS = {
-    "bridge": {
-        "id_n": 0,
-        "type": "bridge",
-        "x": 0,
-        "y": 0,
-        "rotation": 0,
-        "color": ["red", "#ff0000", [255, 0, 0]],
-        "block_matrix": [[1, 1]]
-    },
-    "nut": {
-        "id_n": 0,
-        "type": "nut",
-        "x": 0,
-        "y": 0,
-        "rotation": 0,
-        "color": ["red", "#ff0000", [255, 0, 0]],
-        "block_matrix": [[1]]
-    },
-    "washer": {
-        "id_n": 0,
-        "type": "washer",
-        "x": 0,
-        "y": 0,
-        "rotation": 0,
-        "color": ["red", "#ff0000", [255, 0, 0]],
-        "block_matrix": [[1]]
-    },
-    "screw": {
-        "id_n": 0,
-        "type": "screw",
-        "x": 0,
-        "y": 0,
-        "rotation": 0,
-        "color": ["red", "#ff0000", [255, 0, 0]],
-        "block_matrix": [[1]]
-    }
-}
-
-COLORS = {
-    "green": ["green", "#008000", [0, 128, 0]],
-    "red":  ["red", "#ff0000", [255, 0, 0]],
-    "yellow" : ["yellow", "#ffa500", [255, 165, 0]],
-    "blue": ["blue", "#1b4ccd", [27, 76, 205]]
-}
-
-
 EMPTYSTATE = json.loads(Path(f"{ROOT}/data/empty_state.json").read_text())
 SELECTIONSTATE = json.loads(Path(f"{ROOT}/data/selection_state.json").read_text())
 STATES = Path(f"{ROOT}/data/states.json")
