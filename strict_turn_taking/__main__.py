8896aef71f6188171898f807dad8b93644e0d7fc
seba <seba.g90@gmail.com>
2023-08-08 14:38:20 +0200

    remove todo

--- strict_turn_taking/__main__.py
+++ strict_turn_taking/__main__.py
@@ -17,11 +17,7 @@ class RoomTimer:
         self.start_timer()
 
     def start_timer(self):
-        self.timer = Timer(
-            TIMEOUT_TIMER*60,
-            self.function,
-            args=[self.room_id]
-        )
+        self.timer = Timer(TIMEOUT_TIMER * 60, self.function, args=[self.room_id])
         self.timer.start()
 
     def reset(self):
@@ -41,9 +37,7 @@ class StrictTurnTakingBot(TaskBot):
         room_id = data["room"]
         self.users_per_room[room_id] = list()
 
-        self.timers_per_room[room_id] = RoomTimer(
-            self.close_room, room_id
-        )
+        self.timers_per_room[room_id] = RoomTimer(self.close_room, room_id)
 
         # assign random writing rights and inform the users
         rights = [True, False]
@@ -60,7 +54,7 @@ class StrictTurnTakingBot(TaskBot):
                         "room": data["room"],
                         "message": "You can send a message to your partner",
                         "receiver_id": usr["id"],
-                    }
+                    },
                 )
             else:
                 self.sio.emit(
@@ -69,17 +63,26 @@ class StrictTurnTakingBot(TaskBot):
                         "room": data["room"],
                         "message": "You will only be able to send a message after your partner",
                         "receiver_id": usr["id"],
-                    }
+                    },
                 )
 
+                # make input field unresponsive
                 response = requests.patch(
                     f"{self.uri}/rooms/{room_id}/attribute/id/text",
-                    json={"attribute": "style", "value": "pointer-events: none", "receiver_id": usr["id"]},
+                    json={
+                        "attribute": "style",
+                        "value": "pointer-events: none",
+                        "receiver_id": usr["id"],
+                    },
                     headers={"Authorization": f"Bearer {self.token}"},
                 )
                 response = requests.patch(
                     f"{self.uri}/rooms/{room_id}/attribute/id/text",
-                    json={"attribute": "placeholder", "value": "Wait for a message from your partner", "receiver_id": usr["id"]},
+                    json={
+                        "attribute": "placeholder",
+                        "value": "Wait for a message from your partner",
+                        "receiver_id": usr["id"],
+                    },
                     headers={"Authorization": f"Bearer {self.token}"},
                 )
 
@@ -112,26 +115,21 @@ class StrictTurnTakingBot(TaskBot):
 
     def room_to_read_only(self, room_id):
         """Set room to read only."""
-        # set room to read-only
-        # TODO: comments
+        # set room to read-only by disabling the text input field
         response = requests.patch(
             f"{self.uri}/rooms/{room_id}/attribute/id/text",
             json={"attribute": "readonly", "value": "True"},
             headers={"Authorization": f"Bearer {self.token}"},
         )
-        if not response.ok:
-            logging.error(f"Could not set room to read_only: {response.status_code}")
-            response.raise_for_status()
-
+        self.request_feedback(response, "Could not set room to read_only")
         response = requests.patch(
             f"{self.uri}/rooms/{room_id}/attribute/id/text",
             json={"attribute": "placeholder", "value": "This room is read-only"},
             headers={"Authorization": f"Bearer {self.token}"},
         )
-        if not response.ok:
-            logging.error(f"Could not set room to read_only: {response.status_code}")
-            response.raise_for_status()
+        self.request_feedback(response, "Could not set room to read_only")
 
+        # get users in this room
         response = requests.get(
             f"{self.uri}/rooms/{room_id}/users",
             headers={"Authorization": f"Bearer {self.token}"},
@@ -142,24 +140,25 @@ class StrictTurnTakingBot(TaskBot):
         users = response.json()
         for user in users:
             if user["id"] != self.user:
+                # get current user
                 response = requests.get(
                     f"{self.uri}/users/{user['id']}",
                     headers={"Authorization": f"Bearer {self.token}"},
                 )
-                if not response.ok:
-                    logging.error(f"Could not get user: {response.status_code}")
-                    response.raise_for_status()
+                self.request_feedback(
+                    response, f"Could not get user: {response.status_code}"
+                )
                 etag = response.headers["ETag"]
 
+                # remove this user from this room
                 response = requests.delete(
                     f"{self.uri}/users/{user['id']}/rooms/{room_id}",
                     headers={"If-Match": etag, "Authorization": f"Bearer {self.token}"},
                 )
-                if not response.ok:
-                    logging.error(
-                        f"Could not remove user from task room: {response.status_code}"
-                    )
-                    response.raise_for_status()
+                self.request_feedback(
+                    response,
+                    f"Could not remove user from task room: {response.status_code}",
+                )
                 logging.debug("Removing user from task room was successful.")
 
     def register_callbacks(self):
@@ -182,25 +181,41 @@ class StrictTurnTakingBot(TaskBot):
             self.set_message_privilege(curr_usr["id"], False)
             response = requests.patch(
                 f"{self.uri}/rooms/{room_id}/attribute/id/text",
-                json={"attribute": "style", "value": "pointer-events: none", "receiver_id": curr_usr["id"]},
+                json={
+                    "attribute": "style",
+                    "value": "pointer-events: none",
+                    "receiver_id": curr_usr["id"],
+                },
                 headers={"Authorization": f"Bearer {self.token}"},
             )
             response = requests.patch(
                 f"{self.uri}/rooms/{room_id}/attribute/id/text",
-                json={"attribute": "placeholder", "value": "Wait for a message from your partner", "receiver_id": curr_usr["id"]},
+                json={
+                    "attribute": "placeholder",
+                    "value": "Wait for a message from your partner",
+                    "receiver_id": curr_usr["id"],
+                },
                 headers={"Authorization": f"Bearer {self.token}"},
             )
-            
+
             # assign writing rights to other user
             self.set_message_privilege(other_usr["id"], True)
             response = requests.patch(
                 f"{self.uri}/rooms/{room_id}/attribute/id/text",
-                json={"attribute": "style", "value": "pointer-events: auto", "receiver_id": other_usr["id"]},
+                json={
+                    "attribute": "style",
+                    "value": "pointer-events: auto",
+                    "receiver_id": other_usr["id"],
+                },
                 headers={"Authorization": f"Bearer {self.token}"},
             )
             response = requests.patch(
                 f"{self.uri}/rooms/{room_id}/attribute/id/text",
-                json={"attribute": "placeholder", "value": "Enter your message here!", "receiver_id": other_usr["id"]},
+                json={
+                    "attribute": "placeholder",
+                    "value": "Enter your message here!",
+                    "receiver_id": other_usr["id"],
+                },
                 headers={"Authorization": f"Bearer {self.token}"},
             )
 
@@ -214,6 +229,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # create bot instance
-    echo_bot = StrictTurnTakingBot(args.token, args.user, args.task, args.host, args.port)
+    echo_bot = StrictTurnTakingBot(
+        args.token, args.user, args.task, args.host, args.port
+    )
     # connect to chat server
     echo_bot.run()
