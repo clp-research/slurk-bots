7ec98ec8e4c50c582cec68297b6ea0032635fe90
seba <seba.g90@gmail.com>
2023-09-07 11:23:17 +0200

    adapt taboo bot to inherit from template class

--- taboo/__main__.py
+++ taboo/__main__.py
@@ -1,9 +1,9 @@
-import argparse
 import logging
 import os
 
 import requests
-import socketio
+
+from templates import TaskBot
 
 import random
 
@@ -11,7 +11,30 @@ import random
 LOG = logging.getLogger(__name__)
 
 
-class TabooBot:
+class Session:
+    def __init__(self):
+        self.players = list()
+        self.explainer = None
+        self.word = None
+
+    def close(self):
+        pass
+
+    def pick_explainer(self):
+        self.explainer = random.choice(self.players)["id"]
+
+
+class SessionManager(dict):
+    def create_session(self, room_id):
+        self[room_id] = Session()
+
+    def clear_session(self, room_id):
+        if room_id in self:
+            self[room_id].close()
+            self.pop(room_id)
+
+
+class TabooBot(TaskBot):
     """Bot that manages a taboo game.
 
     - Bot enters a room and starts a taboo game as soon as 2 participants are
@@ -22,42 +45,16 @@ class TabooBot:
     - Solution has been said: end the game, record the winner, start a new game.
     - When new users enter while the game is in progress: make them guessers.
     """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.received_waiting_token = set()
+        self.sessions = SessionManager()
 
-    sio = socketio.Client(logger=True)
-    task_id = None
-    taboo_data = None
-
-    def __init__(self, token, user, host, port):
-        self.token = token
-        self.user = user
-
-        self.uri = host
-        if port is not None:
-            self.uri += f":{port}"
-        self.uri += "/slurk/api"
-
-        LOG.info(f"Running taboo bot on {self.uri} with token {self.token}")
-        # register all event handlers
-        self.register_callbacks()
-
-        self.players_per_room = dict()
-        self.explainer_per_room = dict()
-        self.word_to_guess = dict()
         # TODO: read the game data from file
         self.taboo_data = {
             "Applesauce": ["fruit", "tree", "glass", "preserving"],
             "Beef patty": ["pork", "ground", "steak"],
         }
-
-    def run(self):
-        # establish a connection to the server
-        self.sio.connect(
-            self.uri,
-            headers={"Authorization": f"Bearer {self.token}", "user": self.user},
-            namespaces="/",
-        )
-        # wait until the connection with the server ends
-        self.sio.wait()
 
     @staticmethod
     def message_callback(success, error_msg="Unknown Error"):
@@ -67,10 +64,6 @@ class TabooBot:
         LOG.debug("Sent message successfully.")
 
     def register_callbacks(self):
-        @self.sio.event
-        def joined_room(data):
-            self.user = data["user"]
-
         @self.sio.event
         def user_message(data):
             LOG.debug("Received a user_message.")
@@ -88,6 +81,12 @@ class TabooBot:
             event = data["type"]
             user = data["user"]
 
+            # create this room if not already present
+            if room_id not in self.sessions:
+                self.sessions.create_session(room_id)
+
+            this_session = self.sessions[room_id]
+
             # don't do this for the bot itself
             if user["id"] == self.user:
                 return
@@ -103,16 +102,11 @@ class TabooBot:
                     },
                 )
 
-                # keep track of users per room
-                if not self.players_per_room.get(room_id):
-                    self.players_per_room[room_id] = []
-                    self.explainer_per_room[room_id] = None
-
-                self.players_per_room[room_id].append(
+                this_session.players.append(
                     {**user, "status": "joined", "wins": 0}
                 )
 
-                if len(self.players_per_room[room_id]) < 2:
+                if len(this_session.players) < 2:
                     self.sio.emit(
                         "text",
                         {"message": "Let's wait for more players.", "room": room_id},
@@ -121,25 +115,26 @@ class TabooBot:
                     # TODO: check whether a game is already in progress
                     # start a game
                     # 1) Choose a word
-                    self.word_to_guess[room_id] = random.choice(
+                    this_session.word_to_guess = random.choice(
                         list(self.taboo_data.keys())
                     )
                     # 2) Choose an explainer
-                    self.explainer_per_room[room_id] = random.choice(
-                        self.players_per_room[room_id]
-                    )["id"]
+                    this_session.pick_explainer()
+
                     # 3) Tell the explainer about the word
+                    word_to_guess = this_session.word_to_guess
+                    taboo_words = ", ".join(self.taboo_data[word_to_guess])
                     self.sio.emit(
                         "text",
                         {
-                            "message": f"Your task is to explain the word {self.word_to_guess[room_id]}. You cannot use the following words: {self.taboo_data[self.word_to_guess[room_id]]}",
+                            "message": f"Your task is to explain the word {word_to_guess}. You cannot use the following words: {taboo_words}",
                             "room": room_id,
-                            "receiver_id": self.explainer_per_room[room_id],
+                            "receiver_id": this_session.explainer,
                         },
                     )
                     # 4) Tell everyone else that the game has started
-                    for player in self.players_per_room[room_id]:
-                        if player["id"] != self.explainer_per_room[room_id]:
+                    for player in this_session.players:
+                        if player["id"] != this_session.explainer:
                             self.sio.emit(
                                 "text",
                                 {
@@ -155,13 +150,14 @@ class TabooBot:
                     {"message": f"{user['name']} has left the game.", "room": room_id},
                 )
 
-                self.players_per_room[room_id] = {
-                    player
-                    for player in self.players_per_room[room_id].keys()
-                    if player["id"] != user["id"]
-                }
+                # remove this user from current session
+                this_session.players = list(
+                    filter(
+                        lambda player: player["id"] != user["id"], this_session.players
+                    )
+                )
 
-                if len(self.players_per_room[room_id]) < 2:
+                if len(this_session.players) < 2:
                     self.sio.emit(
                         "text",
                         {
@@ -182,15 +178,17 @@ class TabooBot:
             room_id = data["room"]
             user_id = data["user"]["id"]
 
+            this_session = self.sessions[room_id]
+
             if user_id == self.user:
                 return
 
             # explainer or guesser?
-            if self.explainer_per_room[room_id] == user_id:
+            if this_session.explainer == user_id:
                 LOG.debug(f"{data['user']['name']} is the explainer.")
 
                 # check whether the user used a forbidden word
-                for taboo_word in self.taboo_data[self.word_to_guess[room_id]]:
+                for taboo_word in self.taboo_data[this_session.word_to_guess]:
                     if taboo_word in data["message"]:
                         self.sio.emit(
                             "text",
@@ -199,11 +197,12 @@ class TabooBot:
                                 "room": room_id,
                             },
                         )
-            elif self.word_to_guess[room_id] in data["message"]:
+
+            elif this_session.word_to_guess in data["message"]:
                 self.sio.emit(
                     "text",
                     {
-                        "message": f"{self.word_to_guess[room_id]} was correct!",
+                        "message": f"{this_session.word_to_guess} was correct!",
                         "room": room_id,
                     },
                 )
@@ -214,33 +213,17 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(message)s")
 
     # create commandline parser
-    parser = argparse.ArgumentParser(description="Run Taboo Bot.")
+    parser = TabooBot.create_argparser()
 
-    # collect environment variables as defaults
-    if "BOT_TOKEN" in os.environ:
-        token = {"default": os.environ["BOT_TOKEN"]}
-    else:
-        token = {"required": True}
-    if "BOT_ID" in os.environ:
-        user = {"default": os.environ["BOT_ID"]}
-    else:
-        user = {"required": True}
-    host = {"default": os.environ.get("SLURK_HOST", "http://localhost")}
-    port = {"default": os.environ.get("SLURK_PORT")}
-    taboo_data = {"default": os.environ.get("TABOO_DATA")}
-
-    # register commandline arguments
-    parser.add_argument("-t", "--token", help="token for logging in as bot", **token)
-    parser.add_argument("-u", "--user", help="user id for the bot", **user)
     parser.add_argument(
-        "-c", "--host", help="full URL (protocol, hostname) of chat server", **host
+        "--taboo_data",
+        help="json file containing words",
+        default=os.environ.get("TABOO_DATA")
     )
-    parser.add_argument("-p", "--port", type=int, help="port of chat server", **port)
-    parser.add_argument("--taboo_data", help="json file containing words", **taboo_data)
     args = parser.parse_args()
 
     # create bot instance
-    taboo_bot = TabooBot(args.token, args.user, args.host, args.port)
+    taboo_bot = TabooBot(args.token, args.user, args.task, args.host, args.port)
     # taboo_bot.taboo_data = args.taboo_data
     # connect to chat server
     taboo_bot.run()
