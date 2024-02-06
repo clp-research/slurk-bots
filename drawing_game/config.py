867b2d94ef604d6c9182d749fcc943eb6d703589
Sandra Sanchez <sandra.sanchez.paez@uni-potsdam.de>
2024-02-06 14:48:27 +0100

    Change waiting timer to 5 for testing

--- drawing_game/config.py
+++ drawing_game/config.py
@@ -17,7 +17,7 @@ STARTING_POINTS = 0
 
 TIMEOUT_TIMER = 5  # 5 minutes of inactivity before the room is closed automatically
 LEAVE_TIMER = 3  # 3 minutes if a user is alone in a room
-WAITING_PARTNER_TIMER = 10  # 10 minutes a user waits for a partner
+WAITING_PARTNER_TIMER = 5  # 10 minutes a user waits for a partner
 
 N = 1
 
