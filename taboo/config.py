f454d1231e10f64c28b88bbb8986a1e8aacbd818
Sandra Sanchez <sandra.sanchez.paez@uni-potsdam.de>
2024-02-13 12:00:26 +0100

    Edit timer variables in config so bot can be correctly updated on the server

--- taboo/config.py
+++ taboo/config.py
@@ -17,9 +17,9 @@ LEVEL_WORDS = f"{ROOT}/taboo/data/level_words.json"
 WORDS_PER_ROOM = 6  # -1 to load entire dataset
 STARTING_POINTS = 0
 
-TIMEOUT_TIMER = 1.5  # 5 minutes of inactivity before the room is closed automatically
-LEAVE_TIMER = 0.5  # 3 minutes if a user is alone in a room
-WAITING_PARTNER_TIMER = 1.5  # 10 minutes a user waits for a partner
+TIMEOUT_TIMER = 5  # 5 minutes of inactivity before the room is closed automatically
+LEAVE_TIMER = 3  # 3 minutes if a user is alone in a room
+WAITING_PARTNER_TIMER = 10  # 10 minutes a user waits for a partner
 
 # base html messages with common colors
 COLOR_MESSAGE = '<a style="color:{color};">{message}</a>'
